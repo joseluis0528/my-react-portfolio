d5b3b3440db19da8855ebabaebbabc8850ee4fe5
Jose Mirafuentes <joseluis0528@gmail.com>
2025-05-17 15:59:40 -0500

    Added about me section and info

--- src/About.jsx
+++ src/About.jsx
@@ -1,4 +1,13 @@
 
-function About() {}
+function About() {
+    return (
+        <section id="about">
+        <h2>About Me</h2>
+        <p>Hello, my name is Jose and I am a web developer with a passion for creating beautiful and functional websites.</p>
+        <p>I have experience in HTML, CSS, JavaScript, React, and C++.</p>
+        <p>My goal is to continue learning and obtain my bachelors degree in Compter Science.</p>
+        </section>
+    );
+}
 
 export default About;
