f372d517158dea84239ca850b9b5f21a1b1be4b9
Jose Mirafuentes <joseluis0528@gmail.com>
2025-05-17 15:48:51 -0500

    Added button to card

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,6 +6,7 @@ function Projects() {
             <img className="card-image" src={weathericon} alt="project image"></img>
             <h2 className="card-title" >Project 1</h2>
             <p className="card-text" >This is my first project</p>
+            <button className="card-button">View project</button>
         </div>
     );
 }
