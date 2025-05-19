5fbf13c15f6a2ef5b542b300a36f29c372ad3e39
Jose Mirafuentes <joseluis0528@gmail.com>
2025-05-18 23:13:15 -0500

    Added Contact.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Projects from "./Projects.jsx";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <About />
       <Projects />
+      <Contact />
       <Footer />
     </>
   )
