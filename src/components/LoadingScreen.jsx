11957e38f7d86f4abffd6fc43c5d3e3585f3f10a
Jose Mirafuentes <joseluis0528@gmail.com>
2025-05-21 16:02:05 -0500

    Added useEffect Hook

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"; //4.2k (gzipped: 1.8k)
 
 function LoadingScreen() {
+
+  const [text, setText] = useState("");
+  const fullText = "<Hello World />";
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+    setText(fullText.substring(0, index));
+    index++;
+
+    if (index > fullText.length) {
+      clearInterval(interval);
+    }
+  }, 100);
+
+  return () => clearInterval(interval);
+})
+
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
       <div className="mb-4 text-4xl font-mono font-bold"></div>
-      Hello World <span className="animate-blink"> | </span>
+      { text } <span className="animate-blink"> | </span>
     </div>
   );
 }
