eab801a8d43b81c49759c897c2f1d6373d9f3c6f
Jose Mirafuentes <joseluis0528@gmail.com>
2025-05-22 17:46:14 -0500

    Modified component

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,6 +1,24 @@
 
 export const Home = () => {
   return (
-    <a href="#home" className="text-gray-300 hover:text-white trasition-colors">Home</a>
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
+      <div className="text-center z-10 px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-600 to-purple-400 bg-clip-text text-transparent leading-right">Hello, I am Jose Tech</h1>
+      </div>
+    </section>
   );
 }
+
+
+
+
+
+          // <h1 className="text-5xl md:text-7xl font-bold text-white">
+          //   Hello, I'm <span className="text-blue-500">Jose</span>
+          // </h1>
+          // <p className="mt-4 text-lg md:text-xl text-gray-400">
+          //   A passionate developer with a love for coding and technology.
+          // </p>
+          // <a href="#about" className="mt-6 inline-block px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition duration-300">
+          //   Learn More
+          // </a>
