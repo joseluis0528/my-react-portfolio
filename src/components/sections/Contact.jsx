78825b31419e5178ead6e3967ac8329f66128234
Jose Mirafuentes <joseluis0528@gmail.com>
2025-05-21 21:16:21 -0500

    Updated to arrow function

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,13 +1,6 @@
 
-function Contact() {
+export const Contact = () => {
   return (
-    <section className="contact">
-      <h2>Contact Me</h2>
-      <p>If you would like to get in touch, please feel free to reach out via email or connect with me on LinkedIn.</p>
-      <p><a href="mailto:mirafuentesjose@outlook.com">Email</a></p>
-      <p><a href="https://www.linkedin.com/in/joselmirafuentes/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-    </section>
+    <a href="#contact" className="text-gray-300 hover:text-white trasition-colors">Contact</a>
   );
 }
-
-export default Contact;
