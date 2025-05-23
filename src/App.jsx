fdf27145067b863cfc9b02717dab6be2a01b1552
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2025-05-23 15:24:17 -0500

    Updated app

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import "./App.css";
-// import { Header } from "./components/sections/Header.jsx";
-// import { Footer } from "./components/sections/Footer.jsx";
 import "./index.css";
+import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About";
+import { Projects } from "./components/sections/Projects";
 
 function App() {
 
@@ -18,11 +19,10 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-700 ${isloaded ? "opacity-100" : "opacity-0"} bg-black text--gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+        <About />
+        <Projects />
       </div>
-      {/* <div>
-        <Header />
-        <Footer />
-      </div> */}
     </>
   )
 }
