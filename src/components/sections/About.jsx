c059e5ae694324e3a6cdccde2c70b6fbaa6588ec
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2025-05-24 22:50:51 -0500

    Added reveal on scroll effect to component

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,4 @@
+import { RevealOnScroll } from "./RevealOnScroll";
 
 export const About = () => {
 
@@ -6,15 +7,16 @@ export const About = () => {
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
+            <RevealOnScroll>
             <div className="max-w-3xl mx-auto px-2">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-600 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
-                <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                
+                <div className="glass rounded-xl p-8 border-white/20 border hover:-translate-y-1 transition-all">
                     <p className="text-gray-300 mb-6">
                         I am a first year student at Waubonsee Community College, studying Computer Science. 
                         I am passionate about technology and coding. 
                         I love to learn new things and work on exciting projects.
                     </p>
-
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div className="rounded-xl p-6 hover:-tranlate-y-1 transition-all">
                             <h3 className="text-xl font-bold mb-4">Frontend</h3>
@@ -74,6 +76,7 @@ export const About = () => {
                     </div>
                 </div>
             </div>
+            </RevealOnScroll>
         </section>
     )
 }
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,12 +1,15 @@
+import { RevealOnScroll } from "./RevealOnScroll";
 
 export const Home = () => {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center relative">
+      <RevealOnScroll>
       <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-600 to-purple-400 bg-clip-text text-transparent leading-right">Hello, I am Jose Tech</h1>
-        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-          I am a first year student at Waubonsee Community College, studying Computer Science. 
-          I am passionate about technology and coding. 
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-600 to-cyan-400 bg-clip-text text-transparent leading-right"
+        >Hi, I am Jose</h1>
+        <p className="text-gray-300 text-lg mb-8 max-w-lg mx-auto">
+          I am a first year Computer Science student at Waubonsee Community College.  
+          I am passionate about all things technology. 
           I love to learn new things and work on exciting projects.
         </p>
         <div className="flex justify-center space-x-4">
@@ -14,20 +17,7 @@ export const Home = () => {
           <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59, 130, 246, 0.2)] hover:bg-blue-500/10">Contact Me</a>
         </div>
       </div>
+      </RevealOnScroll>
     </section>
   );
 }
-
-
-
-
-
-          // <h1 className="text-5xl md:text-7xl font-bold text-white">
-          //   Hello, I'm <span className="text-blue-500">Jose</span>
-          // </h1>
-          // <p className="mt-4 text-lg md:text-xl text-gray-400">
-          //   A passionate developer with a love for coding and technology.
-          // </p>
-          // <a href="#about" className="mt-6 inline-block px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition duration-300">
-          //   Learn More
-          // </a>
