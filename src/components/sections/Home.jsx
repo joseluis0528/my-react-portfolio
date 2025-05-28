e301c85b748594c9e38d35c9f39f686822e571ee
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2025-05-27 23:35:56 -0500

    Edited styling and about me info

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -7,10 +7,12 @@ export const Home = () => {
       <div className="text-center z-10 px-4">
         <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-600 to-cyan-400 bg-clip-text text-transparent leading-right"
         >Hi, I am Jose</h1>
-        <p className="text-gray-300 text-lg mb-8 max-w-lg mx-auto">
+        <p className="text-gray-300 text-lg mb-8 max-w-3xl mx-auto">
           I am a first year Computer Science student at Waubonsee Community College.  
-          I am passionate about all things technology. 
-          I love to learn new things and work on exciting projects.
+          I am passionate about programming and creating innovative solutions.
+          I love to learn new technologies and improve my skills every day.
+          I am currently looking for opportunities to apply my knowledge in real-world projects.
+          I am excited to connect with like-minded individuals and explore new challenges.
         </p>
         <div className="flex justify-center space-x-4">
           <a href="#projects" className="bg-blue-700 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59, 130, 246, 0.4)]">View Projects</a>
