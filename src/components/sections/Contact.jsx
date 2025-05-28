86c625044d95c4b0fac79b44d657a7457924d37e
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2025-05-27 23:40:07 -0500

    Added required attribute

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -9,17 +9,17 @@ export const Contact = () => {
           <form className="bg-gray-800 p-6 rounded-lg shadow-md">
             <div className="mb-4">
               <label htmlFor="name" className="block text-gray-300 mb-2">Name</label>
-              <input type="text" id="name" className="w-full p-2 border border-gray-700 rounded focus:outline-none focus:ring focus:ring-blue-500"
+              <input type="text" id="name" required className="w-full p-2 border border-gray-700 rounded focus:outline-none focus:ring focus:ring-blue-500"
               placeholder="Name..." />
             </div>
             <div className="mb-4">
               <label htmlFor="email" className="block text-gray-300 mb-2">Email</label>
-              <input type="email" id="email" className="w-full p-2 border border-gray-700 rounded focus:outline-none focus:ring focus:ring-blue-500" 
+              <input type="email" id="email" required className="w-full p-2 border border-gray-700 rounded focus:outline-none focus:ring focus:ring-blue-500" 
               placeholder="example@gmail.com"/>
             </div>
             <div className="mb-4">
               <label htmlFor="message" className="block text-gray-300 mb-2">Message</label>
-              <textarea id="message" rows="4" className="w-full p-2 border border-gray-700 rounded focus:outline-none focus:ring focus:ring-blue-500"
+              <textarea id="message" required rows="4" className="w-full p-2 border border-gray-700 rounded focus:outline-none focus:ring focus:ring-blue-500"
               placeholder="Your message..."></textarea>
             </div>
             <button type="submit" className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200">Send Message</button>
